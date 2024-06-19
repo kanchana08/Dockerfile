ed9f42a69314f76cc7949aa5f44f3191d1c81dcf
kanchana08 <127290267+kanchana08@users.noreply.github.com>
2024-06-19 16:19:13 +0530

    Update hellowWorld.groovy

--- vars/hellowWorld.groovy
+++ vars/hellowWorld.groovy
@@ -1,37 +1,8 @@
 def call() {
-  pipeline {
-    agent any
-    
-    stages{
-        stage("git_checkout") {
-            steps{
-                 git credentialsId: 'GIT_PATH', url: 'https://github.com/kanchana08/Dockerfile_python.git'
-            }
-        }
-        
-        stage('checking'){
-   
-       when {
-                expression{ 
-                    
-                    env.BRANCH_NAME == 'master' || env.BRANCH_NAME == 'dev' 
-                    
-                    
-                } 
-           
-       }
-
-            steps { 
-
-
-                echo "testing the application....."
-
-            } 
-
-        } 
-   }
-    
+when {
+  expression{ 
+    env.BRANCH_NAME == 'master' || env.BRANCH_NAME == 'dev' 
+  } 
+}
 }
 
-
-}
