ae080568eeef98d3598ae7c6bf51623e177e1657
kanchana08 <127290267+kanchana08@users.noreply.github.com>
2024-06-19 15:01:45 +0530

    Update hellowWorld.groovy

--- vars/hellowWorld.groovy
+++ vars/hellowWorld.groovy
@@ -1,3 +1,3 @@
 def call() {
-  sh'git credentialsId: 'GIT_PATH', url: 'https://github.com/kanchana08/Dockerfile_python.git'
+  sh'git credentialsId: 'GIT_PATH', url: 'https://github.com/kanchana08/Dockerfile_python.git''
 }
